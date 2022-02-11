11790dd2770988333b56fa69c03651f4d9ac7123
Kelvin-kfl33 <kelvin233123@gmail.com>
2022-02-11 03:41:50 +0000

    Task 1E

--- Task1E.py
+++ Task1E.py
@@ -3,7 +3,8 @@ from floodsystem.stationdata import build_station_list
 
 def run():
     stations = build_station_list()
-    x = rivers_by_station_number(stations, 9)  ## shows rivers with top 9 number of stations, not the top 9 position in the river list
+    x = rivers_by_station_number(stations, 9)  
+    print("\nRivers with top 9 number of stations, not the top 9 position in the river list:")
     print(x)
     N = 1
     return x, N
