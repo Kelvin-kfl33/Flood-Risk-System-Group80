a3ecd5f5e69d1045714e6ab70ae0b5cd4bd8db05
Kelvin-kfl33 <kelvin233123@gmail.com>
2022-02-11 02:06:58 +0000

    1F

---
+++ Task1F.py
@@ -0,0 +1,14 @@
+from unicodedata import name
+from floodsystem.station import MonitoringStation
+from floodsystem.station import inconsistent_typical_range_stations
+from floodsystem.stationdata import build_station_list
+
+def run():
+    stations = build_station_list()
+    stations = build_station_list()
+    inconsistent_stations = inconsistent_typical_range_stations(stations)
+    print(inconsistent_stations)
+
+if __name__ == "__main__":
+    print(" Task1F")
+    run()
