58b593e6a54dd1377cee16f5d7e2c760137ff761
Kelvin-kfl33 <kelvin233123@gmail.com>
2022-02-11 03:16:21 +0000

    1D update

--- Task1D.py
+++ Task1D.py
@@ -15,11 +15,7 @@ def run():
     print(ans1)
     print(ans2)
     print(ans3)
-    """To check the length of each list output"""
-    assert len(y) == 10
-    assert len(ans1) == 24
-    assert len(ans2) == 7 
-    assert len(ans3) == 54
+    return y, ans1, ans2 ,ans3
 
 if __name__ == "__main__":
     print(" Task1C")
