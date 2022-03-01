930f909ce9ee837440d26968fda3d7f13263114e
Kelvin-kfl33 <kelvin233123@gmail.com>
2022-03-01 23:33:12 +0000

    update 2E + plot

---
+++ Task2E.py
@@ -0,0 +1,6 @@
+
+from floodsystem.plot import plot_water_levels
+from floodsystem.stationdata import build_station_list
+
+
+plot_water_levels(None ,(2022,1,1), None )
--- floodsystem/plot.py
+++ floodsystem/plot.py
@@ -1,16 +1,29 @@
 import matplotlib.pyplot as plt
-from datetime import datetime, timedelta
-
+import datetime
+from floodsystem.stationdata import build_station_list
 from floodsystem.datafetcher import fetch_measure_levels
 
-def dates_levels(station):
-    dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = dt))
-    return dates, levels 
-    
-def plot_water_levels(station, dates, levels):
-    dt = 10
-    dates, levels = fetch_measure_levels(station.measure_id, dt = datetime.timedelta(days = dt))
 
-    level = []
-    time = [datetime(2022, 2, 1 + dt) for dt in range(0,30)]
+def plot_water_levels(station, dates, levels):
+    
+    stations = build_station_list()
+    station_name = "Cam"
+    station_cam = None
+    for station in stations:
+        if station.name == station_name:
+            station_cam = station
+            break
+
+    if not station_cam:
+        print("Station {} could not be found".format(station_name))
+        return
+
+    dt = 10
+    dates, levels = fetch_measure_levels(
+        station_cam.measure_id, dt=datetime.timedelta(days=dt))
+    
+    plt.plot(dates, levels)
+    plt.xlabel('date')
+    plt.ylabel('water level (m)')
+    plt.show()
 
