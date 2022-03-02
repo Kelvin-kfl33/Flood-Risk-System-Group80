456da50ab12e9ea741fb1c18cfa2ba521620fcdf
Kelvin-kfl33 <kelvin233123@gmail.com>
2022-03-02 14:36:14 +0000

    2B

--- Task2B.py
+++ Task2B.py
@@ -1,12 +1,27 @@
-from floodsystem.station import MonitoringStation
-from floodsystem.stationdata import build_station_list
+from floodsystem.stationdata import build_station_list, update_water_levels
 from floodsystem.flood import stations_level_over_threshold
 
-def run():
 
+def run():
+    # Build list of stations
     stations = build_station_list()
-    stations_level_over_threshold(stations , tol = 1.5)
+
+    # Update latest level data for all stations
+    update_water_levels(stations)
+
     
+    # Alternative implementation
+    # for station in [s for s in stations if s.name in names]:
+    #     print("Station name and current level: {}, {}".format(station.name,
+    #                                                           station.latest_level))
+    def relative_water_details(stations, tol=0.8):
+        relative_water_stations = stations_level_over_threshold(stations, tol)
+        for station in relative_water_stations:
+            print(station[0].name, station[1])
+
+    relative_water_details(stations, 0.95)
+
+
 if __name__ == "__main__":
-    print(" Task2B")
+    print("*** Task 2B: CUED Part IA Flood Warning System ***")
     run()
