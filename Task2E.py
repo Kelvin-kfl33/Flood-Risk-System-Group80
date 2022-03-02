1f34bdeead843b91789b5ceb5515eb7594ef98d6
Kelvin-kfl33 <kelvin233123@gmail.com>
2022-03-02 13:38:57 +0000

    2E

--- Task2E.py
+++ Task2E.py
@@ -1,5 +1,9 @@
 
 from floodsystem.plot import plot_water_levels
 
+def run():
+    plot_water_levels(None ,(2022,1,1), None )
 
-plot_water_levels(None ,(2022,1,1), None )
+if __name__ == "__main__":
+    print("*** Task 2E: CUED Part IA Flood Warning System ***")
+    run()
