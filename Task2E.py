c7622749aed8543af9110ca76222247b3413df93
Kelvin-kfl33 <kelvin233123@gmail.com>
2022-03-11 18:55:24 +0000

    2E up

--- Task2E.py
+++ Task2E.py
@@ -1,8 +1,30 @@
+import datetime
 
-from floodsystem.plot import plot_water_levels
+from floodsystem.datafetcher import fetch_measure_levels
+from floodsystem.stationdata import build_station_list, update_water_levels
+from floodsystem.plot import plot_water_level
+from floodsystem.flood import stations_highest_rel_level
+
 
 def run():
-    plot_water_levels(None ,(2022,1,1), None )
+
+    # Build list of stations
+    stations = build_station_list()
+
+    update_water_levels(stations)
+    #Find the 5 stations with highest relative water level
+    
+    def plot_highest_risk(stations, N, dt):
+        """This function retrieves the N most at risk stations and then plots the relative water level from the last 10 days"""
+        stations_to_plot = stations_highest_rel_level(stations, N)
+        for station in stations_to_plot:
+            dates, levels = fetch_measure_levels(station.measure_id, dt=datetime.timedelta(days=dt))
+            plot_water_level(station, dates, levels)
+        return None
+    
+    plot_highest_risk(stations, 5, 10)
+
+
 
 if __name__ == "__main__":
     print("*** Task 2E: CUED Part IA Flood Warning System ***")
